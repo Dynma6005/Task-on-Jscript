c40b7e0bc23557fc41493912c70706bce386cf2c
Dynma6005 <105459928+Dynma6005@users.noreply.github.com>
2022-06-24 02:21:49 +0100

    Add files via upload

---
+++ info.js
@@ -0,0 +1,5 @@
+var name, country, height
+name=prompt("Entrer your name")
+country=prompt("Enter your country")
+height=prompt("Enter your height")
+document.write(name + "<br>" + country + "<br>" + height)
